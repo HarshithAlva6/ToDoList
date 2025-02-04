a7499df0cad5c231a8ba06416738021353c0009f
Harshith <harshalva.titan@csu.fullerton.edu>
2025-02-03 22:52:48 -0800

    Final API URL changes

--- backend/server.js
+++ backend/server.js
@@ -92,6 +92,6 @@ app.delete('/tasks/:id', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
